e320767df4e0b4b64b8b328e8ed56db4fd5133ee
kieukhanhquan <kieukhanhquan@gmail.com>
2024-01-25 12:54:55 +0700

    update cookies

--- common/helpers/setCookies.ts
+++ common/helpers/setCookies.ts
@@ -5,12 +5,32 @@ import { IAuthResponse } from "../interfaces";
 export function setAccessCookies(res: IAuthResponse) {
     console.log(res.access_token)
     Cookies.set(TOKEN, res.access_token, { secure: true });
+    Cookies.set("role", res.role, { secure: true });
 }
 
 export function getAccessCookies() {
     return Cookies.get(TOKEN);
 }
 
+export function getRole() {
+    return Cookies.get("role");
+}
+
+
 export function removeAccessCookies() {
+    Cookies.remove(TOKEN)
+}
+
+
+export function setRoleCookies(res: IAuthResponse) {
+    console.log(res.access_token)
+    Cookies.set(TOKEN, res.access_token, { secure: true });
+}
+
+export function getRoleCookies() {
+    return Cookies.get(TOKEN);
+}
+
+export function removeRoleCookies() {
     Cookies.remove(TOKEN)
 }
